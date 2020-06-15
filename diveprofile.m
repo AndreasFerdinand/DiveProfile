a0fc42de8be4c6b5cbb74fb5c44fdc51f3d47092
Andreas Ferdinand <a.kasper@froeschler.net>
2020-06-15 18:52:03 +0200

    Console Output changed

--- diveprofile.m
+++ diveprofile.m
@@ -1,46 +1,66 @@
 
-workingdir = "./"
-sourcefilename = "depth.txt"
-divelocation = "The Sea"
-divedate = "6.6.2020"
+workingdir = "./";
+sourcefilename = "depth.txt";
+divelocation = "The Sea";
+divedate = "6.6.2020";
 
 
-targetfilepath = [ workingdir strrep( divelocation, " ", "_" ) "-" divedate ".svg" ]
+targetfilepath = [ workingdir strrep( divelocation, " ", "_" ) "-" divedate ".svg" ];
 
-depth = load("-ascii", [workingdir sourcefilename] )
+depth = load("-ascii", [workingdir sourcefilename] );
 
-depthcount = length(depth)
-timelimit = ( depthcount / 2 ) - 0.5
+depthcount = length(depth);
+timelimit = ( depthcount / 2 ) - 0.5;
 
 time = 0:0.5:timelimit;
 
 
-depthdelta = diff(depth)
-depthdelta = [0,depthdelta]
+depthdelta = diff(depth);
+depthdelta = [0,depthdelta];
 
-buckets = [ sum( depth<5 ), sum( 5 <= depth & depth < 10 ), sum( 10 <= depth & depth < 15 ), sum( 15 <= depth & depth < 20 ), sum( 20 <= depth & depth < 25 )  ]
-buckets = buckets * 0.5
+buckets = [ sum( depth<5 ), sum( 5 <= depth & depth < 10 ), sum( 10 <= depth & depth < 15 ), sum( 15 <= depth & depth < 20 ), sum( 20 <= depth & depth < 25 ) ];
+buckets = buckets * 0.5;
 
-divelength = sum( buckets )
+divelength = sum( buckets );
 
-subplot (2, 1, 1)
-plot(time, depth, time, depthdelta, "linestyle",":" )
-grid on
-axis ("ij")
-title([ divelocation ", " divedate ])
-xlabel("Zeit / Minute")
-ylabel("Tiefe / Meter")
+subplot (2, 1, 1);
+plot(time, depth, time, depthdelta, "linestyle",":" );
+grid on;
+axis ("ij");
+title([ divelocation ", " divedate ]);
+xlabel("Zeit / Minute");
+ylabel("Tiefe / Meter");
 legend ("Tiefe","Delta","location","southeast");
 
 
-subplot (2, 1, 2)
+subplot (2, 1, 2);
 bar(buckets,"facecolor", "y", "edgecolor", "b");
-grid on
-xlabel("Tiefenbereich / Meter, obere Schranke nicht im Intervall enthalten")
-ylabel("Dauer / Minute")
-xticklabels({"0-5","5-10","10-15","15-20","20-25"})
+grid on;
+xlabel("Tiefenbereich / Meter, obere Schranke nicht im Intervall enthalten");
+ylabel("Dauer / Minute");
+xticklabels({"0-5","5-10","10-15","15-20","20-25"});
 
-text([1:length(buckets)], buckets'+0.5, num2str(buckets','%0.1f'),'HorizontalAlignment','center','VerticalAlignment','bottom')
-text(0.02,23, ["Gesamtdauer: " num2str(divelength)])
+text([1:length(buckets)], buckets'+0.5, num2str(buckets','%0.1f'),'HorizontalAlignment','center','VerticalAlignment','bottom');
+text(0.02,23, ["Gesamtdauer: " num2str(divelength)]);
 
 saveas (1, targetfilepath);
+
+depth_from = 0;
+depth_to = 5;
+
+
+printf("depth         duration\n");
+printf("[meters]      [minutes]\n");
+printf("========      =========\n");
+
+for x=1:length(buckets)
+
+printf(" %2d - %2d %14.1f\n",depth_from, depth_to,buckets(x));
+
+
+depth_from = depth_from + 5;
+depth_to = depth_to + 5;
+end
+
+
+
